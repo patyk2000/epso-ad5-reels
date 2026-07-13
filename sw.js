c72bae0c815e24c27dd1cba121d53d77d3f4fd17
patyk2000 <101869338+patyk2000@users.noreply.github.com>
2026-07-13 10:37:43 +0200

    v4: full app encrypted behind password (AES-GCM), incl. MSZ collection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "epso-ad5-v2";
+const CACHE = "epso-ad5-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
